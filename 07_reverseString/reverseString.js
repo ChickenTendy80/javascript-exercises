e6bc93333d3706596902bfb69e932867037c4eb4
Ryan Kim <ryanhkim0418@gmail.com>
2025-06-18 15:51:59 -0500

    exercise 7

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    return string.split("").reverse().join("");
+    //split: places each character of the string into an array
+    //reverse: reverses the array of characters
+    //join: joins the elt of the array into a string
 };
 
 // Do not edit below this line

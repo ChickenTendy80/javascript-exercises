7b99b2c4e2f3bfb0644c733637981fea4d67d17c
Ryan Kim <ryanhkim0418@gmail.com>
2025-06-18 15:46:16 -0500

    exercise 6

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    let tmp = "";
+    if(num < 0){
+        return "ERROR";
+    }
+    for(let i = 0; i < num; i++){
+        tmp += str;
+    }
+    return tmp;
 };
 
 // Do not edit below this line

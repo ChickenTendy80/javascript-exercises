990cdfdb2270827fbe9d9c6e15e56bc9ee01b947
Ryan Kim <ryanhkim0418@gmail.com>
2025-06-19 16:06:05 -0500

    exercise 12

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((total, current) => total + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((total, current) => total * current, 1);
 };
 
-const power = function() {
-	
+const power = function(x,y) {
+	return x ** y;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  let tmp = 1;
+	for(let i = 1; i <= n; i++){
+    tmp *= i;
+  }
+  return tmp;
 };
 
 // Do not edit below this line
